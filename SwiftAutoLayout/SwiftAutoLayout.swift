570ce100404cb285b204304767d1d39ec69c356c
David Hoerl <dhoerl@sailthru.com>
2015-10-25 08:48:15 -0700

    Version with original and modified code together - next version will remove the modified code.

--- SwiftAutoLayout/SwiftAutoLayout.swift
+++ SwiftAutoLayout/SwiftAutoLayout.swift
@@ -43,10 +43,24 @@ public struct ALLayoutItem {
     //
     // Repro case: http://cl.ly/3S0a1T0Q0S1D
     // rdar://17412596, OpenRadar: http://www.openradar.me/radar?id=5275533159956480
-    
+
+#if false
     /// Builds a constraint by relating the item to another item.
     func relateTo(right: ALLayoutItem, relation: NSLayoutRelation) -> NSLayoutConstraint {
-        return NSLayoutConstraint(item: view, attribute: attribute, relatedBy: relation, toItem: right.view, attribute: right.attribute, multiplier: right.multiplier, constant: right.constant)
+		let multiplier: CGFloat
+		if self.multiplier == 1 && right.multiplier == 1 {
+			multiplier = 1
+		} else
+		if right.multiplier != 1 && self.multiplier == 1 {
+			multiplier = right.multiplier
+		} else
+		if right.multiplier == 1 && self.multiplier != 1 {
+			multiplier = self.multiplier
+		} else {
+			fatalError("Cannot have both a left and right multiplier")
+		}
+
+        return NSLayoutConstraint(item: view, attribute: attribute, relatedBy: relation, toItem: right.view, attribute: right.attribute, multiplier: multiplier, constant: right.constant - self.constant)
     }
     
     /// Builds a constraint by relating the item to a constant value.
@@ -83,6 +97,59 @@ public struct ALLayoutItem {
     func lessThanOrEqualToConstant(right: CGFloat) -> NSLayoutConstraint {
         return relateToConstant(right, relation: .LessThanOrEqual)
     }
+#else
+///*
+    func relateTo(right: ALLayoutItem, relation: NSLayoutRelation) -> NSLayoutConstraint {
+		let multiplier: CGFloat
+		if self.multiplier == 1 && right.multiplier == 1 {
+			multiplier = 1
+		} else
+		if right.multiplier != 1 && self.multiplier == 1 {
+			multiplier = right.multiplier
+		} else
+		if right.multiplier == 1 && self.multiplier != 1 {
+			multiplier = self.multiplier
+		} else {
+			fatalError("Cannot have both a left and right multiplier")
+		}
+        return NSLayoutConstraint(item: view, attribute: attribute, relatedBy: relation, toItem: right.view, attribute: right.attribute, multiplier: multiplier, constant: right.constant - self.constant)
+    }
+    
+    func relateTo(right: CGFloat, relation: NSLayoutRelation) -> NSLayoutConstraint {
+        return NSLayoutConstraint(item: view, attribute: attribute, relatedBy: relation, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1.0, constant: right)
+    }
+    
+    /// Equivalent to NSLayoutRelation.Equal
+    func equalTo(right: ALLayoutItem) -> NSLayoutConstraint {
+        return relateTo(right, relation: .Equal)
+    }
+    
+    /// Equivalent to NSLayoutRelation.Equal
+    func equalTo(right: CGFloat) -> NSLayoutConstraint {
+        return relateTo(right, relation: .Equal)
+    }
+    
+    /// Equivalent to NSLayoutRelation.GreaterThanOrEqual
+    func greaterThanOrEqualTo(right: ALLayoutItem) -> NSLayoutConstraint {
+        return relateTo(right, relation: .GreaterThanOrEqual)
+    }
+    
+    /// Equivalent to NSLayoutRelation.GreaterThanOrEqual
+    func greaterThanOrEqualTo(right: CGFloat) -> NSLayoutConstraint {
+        return relateTo(right, relation: .GreaterThanOrEqual)
+    }
+    
+    /// Equivalent to NSLayoutRelation.LessThanOrEqual
+    func lessThanOrEqualTo(right: ALLayoutItem) -> NSLayoutConstraint {
+        return relateTo(right, relation: .LessThanOrEqual)
+    }
+    
+    /// Equivalent to NSLayoutRelation.LessThanOrEqual
+    func lessThanOrEqualTo(right: CGFloat) -> NSLayoutConstraint {
+        return relateTo(right, relation: .LessThanOrEqual)
+    }
+//*/
+#endif
 }
 
 
@@ -110,6 +177,15 @@ public func + (left: ALLayoutItem, right: Int) -> ALLayoutItem {
 	return ALLayoutItem(view: left.view, attribute: left.attribute, multiplier: left.multiplier, constant: left.constant + CGFloat(right))
 }
 
+postfix operator +^ {}
+public postfix func +^ (left: ALLayoutItem) -> ALLayoutItem {
+    return ALLayoutItem(view: left.view, attribute: left.attribute, multiplier: left.multiplier, constant: ALLayoutItem.standardConstantBetweenSiblings)
+}
+postfix operator -^ {}
+public postfix func -^ (left: ALLayoutItem) -> ALLayoutItem {
+    return ALLayoutItem(view: left.view, attribute: left.attribute, multiplier: left.multiplier, constant: -ALLayoutItem.standardConstantBetweenSiblings)
+}
+
 /// Subtracts the RHS value from the operand's constant
 public func - (left: ALLayoutItem, right: CGFloat) -> ALLayoutItem {
 	return ALLayoutItem(view: left.view, attribute: left.attribute, multiplier: left.multiplier, constant: left.constant - right)
@@ -118,7 +194,7 @@ public func - (left: ALLayoutItem, right: Int) -> ALLayoutItem {
 	return ALLayoutItem(view: left.view, attribute: left.attribute, multiplier: left.multiplier, constant: left.constant - CGFloat(right))
 }
 
-/// Alow '~' to set the priority, as a trailing operation
+/// Alow '!' to set the priority, as a trailing operation
 infix operator ! { associativity left precedence 100 }
 public func ! (constraint: NSLayoutConstraint, priority: UILayoutPriority) -> NSLayoutConstraint {
     constraint.priority = priority
@@ -128,6 +204,18 @@ public func ! (constraint: NSLayoutConstraint, priority: Int) -> NSLayoutConstra
     constraint.priority = UILayoutPriority(priority)
     return constraint
 }
+/// Support identifier
+infix operator & { associativity left precedence 100 }
+public func & (constraint: NSLayoutConstraint, identifier: String) -> NSLayoutConstraint {
+    constraint.identifier = identifier
+    return constraint
+}
+/// Support active
+//postfix operator -- { precedence 100 }
+public postfix func -- (constraint: NSLayoutConstraint) -> NSLayoutConstraint {
+    constraint.active = false
+    return constraint
+}
 
 
 /// Equivalent to NSLayoutRelation.Equal
@@ -135,6 +223,7 @@ public func == (left: ALLayoutItem, right: ALLayoutItem) -> NSLayoutConstraint {
 	return left.equalTo(right)
 }
 
+#if false
 /// Equivalent to NSLayoutRelation.Equal
 public func == (left: ALLayoutItem, right: CGFloat) -> NSLayoutConstraint {
     return left.equalToConstant(right)
@@ -168,6 +257,45 @@ public func <= (left: ALLayoutItem, right: CGFloat) -> NSLayoutConstraint {
 public func <= (left: ALLayoutItem, right: Int) -> NSLayoutConstraint {
     return left.lessThanOrEqualToConstant(CGFloat(right))
 }
+
+#else
+
+/// Equivalent to NSLayoutRelation.Equal
+public func == (left: ALLayoutItem, right: CGFloat) -> NSLayoutConstraint {
+    return left.equalTo(right)
+}
+public func == (left: ALLayoutItem, right: Int) -> NSLayoutConstraint {
+    return left.equalTo(CGFloat(right))
+}
+
+/// Equivalent to NSLayoutRelation.GreaterThanOrEqual
+public func >= (left: ALLayoutItem, right: ALLayoutItem) -> NSLayoutConstraint {
+	return left.greaterThanOrEqualTo(right)
+}
+
+/// Equivalent to NSLayoutRelation.GreaterThanOrEqual
+public func >= (left: ALLayoutItem, right: CGFloat) -> NSLayoutConstraint {
+    return left.greaterThanOrEqualTo(right)
+}
+public func >= (left: ALLayoutItem, right: Int) -> NSLayoutConstraint {
+    return left.greaterThanOrEqualTo(CGFloat(right))
+}
+
+/// Equivalent to NSLayoutRelation.LessThanOrEqual
+public func <= (left: ALLayoutItem, right: ALLayoutItem) -> NSLayoutConstraint {
+	return left.lessThanOrEqualTo(right)
+}
+
+/// Equivalent to NSLayoutRelation.LessThanOrEqual
+public func <= (left: ALLayoutItem, right: CGFloat) -> NSLayoutConstraint {
+    return left.lessThanOrEqualTo(right)
+}
+public func <= (left: ALLayoutItem, right: Int) -> NSLayoutConstraint {
+    return left.lessThanOrEqualTo(CGFloat(right))
+}
+
+
+#endif
 
 public extension ALView {
     func al_operand(attribute: NSLayoutAttribute) -> ALLayoutItem {
